1dc8f209b79a47e81669e7b3d0f632cb8c24265d
aman <amanhansa90@gmail.com>
2023-02-03 12:01:19 +0530

    first commit

---
+++ src/componets/footer.jsx
@@ -0,0 +1,7 @@
+import "./footer.css"
+export default function Foot1(){
+
+    return( <footer className="footer"> This is basic navigation bar footer</footer>)
+
+
+}
---
+++ src/componets/navbar.jsx
@@ -0,0 +1,7 @@
+import "./navbar.css"
+export default function Nav1(){
+
+    return( <nav className="navdecorate"> This is basic navigation bar</nav>)
+
+
+}
